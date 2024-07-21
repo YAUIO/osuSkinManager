9763ded1425817ac3db77fbabd82be1a02da0576
Yū <artembezzzz@gmail.com>
2024-07-22 01:29:44 +0200

    fixed files being shown in skin list

--- Files.cpp
+++ Files.cpp
@@ -1,16 +1,20 @@
 #include "Files.h"
+#include "fmt/core.h"
 
 std::vector<File> Files::getSkins() {
     auto skins = std::vector<File>();
 
     for (const auto &entry: std::filesystem::directory_iterator(Files::osuPath)) {
-        skins.push_back(File(entry.path().generic_string(), entry.path().filename().generic_string(), true));
+        if (entry.is_directory()) {
+            skins.push_back(
+                    File(entry.path().generic_string(), entry.path().filename().generic_string(), true));
+        }
     }
 
     std::ranges::sort(skins.begin(), skins.end(),
-                      [](std::string const & first, std::string const & second) {
+                      [](std::string const &first, std::string const &second) {
                           int i = 0;
-                          for (char c : first) {
+                          for (char c: first) {
                               if (i >= second.size()) {
                                   return true;
                               }
@@ -23,7 +27,7 @@ std::vector<File> Files::getSkins() {
                           }
                           return true;
                       },
-                      [](File const & f){return f.name;});
+                      [](File const &f) { return f.name; });
 
     return skins;
 }
