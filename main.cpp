18d368a4cfc6554b172373fab6ad82fff13010b5
Yū <artembezzzz@gmail.com>
2024-07-28 15:05:30 +0200

    next and previous group buttons done alongside with proper data recording

--- main.cpp
+++ main.cpp
@@ -148,6 +148,7 @@ int main() {
                                         init = true;
                                         isSkinListReady = false;
                                         isSkinListNew = true;
+                                        displayingGroup = 0;
                                     }
 
                                     break;
